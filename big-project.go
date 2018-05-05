7528de33e5a664a64a9e4e816a83a9e255f6d527
ialmoqren <ialmoqren@gmail.com>
2018-05-06 02:52:37 +0300

    Adding variables

--- big-project.go
+++ big-project.go
@@ -1,30 +1,19 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	""
 )
 
-type SitemapIndex struct {
-	Locations []Location `xml:"sitemap"`
-}
-type Location struct {
-	Loc string `xml:"loc"`
-}
-
-func (l Location) String() string {
-	return fmt.Sprintf(l.Loc)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `<h1>Hey there</h1>
+<p>It's me</p>
+<p>...playing with go!</p>
+<p>My name is %s but you can call me %s</p>
+`, "Ibrahim Almuqrin", "Ibra")
 }
 
 func main() {
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-
-	var s SitemapIndex
-	xml.Unmarshal(bytes, &s)
-	fmt.Println(s.Locations)
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe("", nil)
 }
