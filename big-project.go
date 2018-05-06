fb48cb7f3ebcbc74965b63fae58e9257f9b8c53a
ialmoqren <ialmoqren@gmail.com>
2018-05-06 15:58:15 +0300

    Looping

--- big-project.go
+++ big-project.go
@@ -1,30 +1,18 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"time"
 )
 
-type SitemapIndex struct {
-	Locations []Location `xml:"sitemap"`
-}
-
-type Location struct {
-	Loc string `xml:"loc"`
-}
-
-func (l Location) String() string {
-	return fmt.Sprintf(l.Loc)
-}
-
 func main() {
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
 
-	var s SitemapIndex
-	xml.Unmarshal(bytes, &s)
-	fmt.Println(s.Locations)
+	x := 5
+	for {
+		fmt.Println(x, x, x, time.Now().Format(time.RFC850))
+		x += 3
+		if x > 25 {
+			break
+		}
+	}
 }
