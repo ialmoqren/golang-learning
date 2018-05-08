d32ce385863cae320b4aab749f1846c216817cff
ialmoqren <ialmoqren@gmail.com>
2018-05-08 19:11:22 +0300

    finished but slow

--- big-project.go
+++ big-project.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 )
@@ -21,7 +22,12 @@ type NewsMap struct {
 	Location string
 }
 
-func main() {
+type NewsAggPage struct {
+	Title string
+	News  map[string]NewsMap
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
 	var s SitemapIndex
 	var n News
 	newsMap := make(map[string]NewsMap)
@@ -36,13 +42,21 @@ func main() {
 		bytes, _ := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
 		xml.Unmarshal(bytes, &n)
-		for idx, _ := range n.Titles {
+		for idx, _ := range n.Keywords {
 			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
 		}
 	}
-	for idx, data := range newsMap {
-		fmt.Println("\n\n\n", idx)
-		fmt.Println("\n", data.Keyword)
-		fmt.Println("\n", data.Location)
-	}
+	p := NewsAggPage{Title: "My Amazing News Aggregator", News: newsMap}
+	t, _ := template.ParseFiles("basictemplate.html")
+	fmt.Println(t.Execute(w, p))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/agg/", newsAggHandler)
+	http.ListenAndServe("", nil)
 }
